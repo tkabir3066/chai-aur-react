08f407512fd3b1a4b0540b79c2cf350e77f3ce3d
Tutul Kabir <tutulkabir@gmail.com>
2023-12-13 11:15:32 +0530

    learned about basic react and vite react

---
+++ 01_basic_react/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Hello World | Tutul kabir</h1>;
+}
+
+export default App;
---
+++ 01_vite_react/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Hello World | I am Tutul Kabir</h1>
+    </>
+  );
+}
+
+export default App;
