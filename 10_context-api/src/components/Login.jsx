7052f1ac45f7b79bcf4916b28d1ffb598387e56f
Tutul Kabir <tutulkabir@gmail.com>
2024-06-05 19:27:16 +0530

    context api project part-1 end

---
+++ 10_context-api/src/components/Login.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from "react";
+import UserContext from "../context/UserContext";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const context = useContext(UserContext);
+  const { setUser } = context;
+  const handleSubmit = () => {
+    setUser({ username, password });
+  };
+  return (
+    <div>
+      <h1>Login</h1>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="username"
+      />{" "}
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default Login;
